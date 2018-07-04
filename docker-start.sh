2c5c042bb61eb334e88e0dd1bc4be8453207cbd7
Chris <electronicsleep@gmail.com>
2018-07-03 18:54:40 -0700

    systemcapture container

--- docker-start.sh
+++ docker-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 GOOS=linux go build SystemCapture.go
-docker rm scapp
-docker build -t scapp .
-docker run -p 5000:5000 --name scapp -i -t scapp
+docker rm systemcapture
+docker build -t systemcapture .
+docker run -p 5000:5000 --name systemcapture -i -t systemcapture
