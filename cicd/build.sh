12c6381c8c150ea5bdd33a5ed1d88003ea2cc121
Chris <electronicsleep@gmail.com>
2022-09-25 14:02:49 -0700

    cicid-build

--- cicd/build.sh
+++ cicd/build.sh
@@ -2,7 +2,6 @@
 mkdir bin
 set -ex
 echo "Building for Linux and MacOS"
-gofmt -w SystemCapture.go
 GOOS=linux go build -o bin/SystemCapture SystemCapture.go
 GOOS=darwin go build -o bin/SystemCapture-MacOS SystemCapture.go
 echo "Done"
