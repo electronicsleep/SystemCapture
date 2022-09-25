ea9f33095cdb0f99d724aa5ace2a6935610b591b
Chris <electronicsleep@gmail.com>
2022-09-25 14:04:19 -0700

    cicd-build

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir bin
 set -ex
+export PATH=$PATH:/usr/local/go/bin
+go version
 echo "Building for Linux and MacOS"
 GOOS=linux go build -o bin/SystemCapture SystemCapture.go
 GOOS=darwin go build -o bin/SystemCapture-MacOS SystemCapture.go
